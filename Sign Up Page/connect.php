9046b4efe368539ba5962b6715a6f165316c3eab
Amir <35382927+amir-Nau@users.noreply.github.com>
2018-12-02 16:40:54 -0600

    Updates

--- Sign Up Page/connect.php
+++ Sign Up Page/connect.php
@@ -1,9 +1,59 @@
 <?php 
 
-  mysqli_connect("ftp.stackcp.com", "MarioRangers-3935ddfa", "160411581Ab");
 
-  echo mysqli_connect_error();
+$username = filter_input(INPUT_POST, 'username');
+ $firstName = filter_input(INPUT_POST, 'firstName');
+ $lastName = filter_input(INPUT_POST, 'lastName');
+ if (!empty($username)){
+if (!empty($lastName))
+if (!empty($firstName)){
+$host = "shareddb-j.hosting.stackcp.net";
+$dbusername = "MarioRangers-3935ddfa";
+$dbpassword = "160411581Ab";
+$dbname = "MarioRangers-3935ddfa";
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+else{
+  $sql = "INSERT INTO users (username, firstName, lastName)
+  values ('$username','$firstName','$lastName')";
+  if ($conn->query($sql)){
+    echo "New record is inserted sucessfully";
+  }
+  else{
+    echo "Error: ". $sql ."
+". $conn->error;
+  }
+  $conn->close();
+}
+}
+else{
+  echo "Password should not be empty";
+  die();
+}
+ }
+ else{
+  echo "Username should not be empty";
+  die();
+ }
   
+  if (isset($_REQUEST["submit"])) {
+  		$username = $_REQUEST["username"]
+
+  		$rowcount = mysql_query("select * from users where username = '$username'")
+  }
+
+  if ($rowcount == true) {
+ 		echo "Yaya"
+  }
+  else{
+  	echo "Nay"
+  }
 ?>
 
 
